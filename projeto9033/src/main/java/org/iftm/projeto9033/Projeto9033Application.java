979e6cc8db0cb5c55cb9be03f54f2804056165a1
baboballer <ivanirvictory@gmail.com>
2025-04-08 20:58:05 -0300

    criando o meu projeto

---
+++ projeto9033/src/main/java/org/iftm/projeto9033/Projeto9033Application.java
@@ -0,0 +1,13 @@
+package org.iftm.projeto9033;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto9033Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto9033Application.class, args);
+	}
+
+}
---
+++ projeto9033/src/test/java/org/iftm/projeto9033/Projeto9033ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iftm.projeto9033;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto9033ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
